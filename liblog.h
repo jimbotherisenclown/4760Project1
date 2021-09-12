a5c99bbaa85267d8be462d119b9bdf77640e45bf
Benjamin West <jimbotherisenclown@gmail.com>
2021-09-12 00:11:13 -0500

    Renamed log.h to liblog.h. Added libtemplate.c

---
+++ liblog.h
@@ -0,0 +1,21 @@
+#include <time.h>
+
+typedef struct data_struct {
+        time_t  time;
+        char    type;
+	char *  string;
+} data_t;
+
+//Save a message at the end of a list & log time of the message. Return 0 on success / -1 on failure.
+//Set errno (part of perror on failure).
+int    addmsg( const char type, const char * msg );
+
+//Release storage allocated for logged message & empty list of logged messages.
+void   clearlog();
+
+//Allocate space for a string containing the entire log, copy the log into the string, and return a 
+//pointer to the string. Return NULL on unsuccessful invocation. Set errno (part of perror) on failure.
+char * getlog();
+
+//Save to a diskfile. Return 0 on success / -1 on failure. Set errno (part of perror) on failure.
+int    savelog (char * filename )
